abaea4571ea9ff3a360c075229e451147c1e2b8b
Fenext04 <mateusmelo04@gmail.com>
2021-06-30 21:10:58 -0300

    Atualização de Site de Noticias

--- Python/gerador_cpfPOO/main.py
+++
@@ -1,9 +0,0 @@
-from gerador_cpf import GeradorCpf
-
-cpf = GeradorCpf()
-
-contador = int(input("Informe a quantidade de cpfs válidos que vc deseja gerar: "))
-with open("Cpfs.txt","w") as arquivo:
-    for c in range(contador):
-        cpf.gerar_cpf()
-        arquivo.write(cpf.converter_cpf_em_texto())
